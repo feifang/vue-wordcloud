efa4de9b9db07f4cca748ca4a97e315745dd5cda
fangfei <karen.feifang@gmail.com>
2018-11-11 11:54:22 +0800

    chore: update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path')
 var webpack = require('webpack')
+
 function resolve(dir) {
   return path.join(__dirname, '..', dir)
 }
@@ -14,8 +15,7 @@ module.exports = {
     umdNamedDefine: true
   },
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.(js|vue)$/,
         loader: 'eslint-loader',
         enforce: 'pre',
@@ -32,8 +32,8 @@ module.exports = {
             // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
             // the "scss" and "sass" values for the lang attribute to the right configs here.
             // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': 'vue-style-loader!css-loader!sass-loader',
-            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+            // 'scss': 'vue-style-loader!css-loader!sass-loader',
+            // 'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
           }
           // other vue-loader options go here
         }
